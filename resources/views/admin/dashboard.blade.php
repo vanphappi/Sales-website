e1567b97c11033372e178ce9b144b87db9a43c98
Huỳnh Văn Pháp <19521987@gm.uit.edu.vn>
2022-01-14 18:28:22 +0700

    d
    d

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('admin_layout')
-@section('admin_contend')
-<h3>Chào mừng bạn đến với admin</h3>
-@endsection
--- resources/views/livewire/cart-counter.blade.php
+++
@@ -1,5 +0,0 @@
-<a class="btn" type="button" href="{{ route('showpagecart') }}">
-    <i class="fas fa-shopping-cart" aria-hidden="true"></i>
-        @livewire('cart-counter')
-        Cart ({{ $cart_count }})
-</a> 
